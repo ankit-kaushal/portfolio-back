6eb3c1d16eb998612e20158d4f024a24bcc69a00
Ankit Kaushal <ankitkaushal288@gmail.com>
2024-05-04 12:22:20 +0530

    updated name

--- src/routers/profile.js
+++ src/routers/profile.js
@@ -7,7 +7,7 @@ router.get("/", async (req, res) => {
     try {
         const getUsers = await UserData.findOne({}, { _id: 0, createdAt: 0, updatedAt: 0 }).sort({ updatedAt: -1 });
         const getProjects = await ProjectData.find();
-        res.json({ users: getUsers, projects: getProjects });
+        res.json({ user: getUsers, projects: getProjects });
     } catch (error) {
         console.error(error);
         res.status(500).send("Internal Server Error");
